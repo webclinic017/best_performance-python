92e42e87bad8a8ebef655a3d8c2aba0cf2f2aaad
seongpil0948 <seongpil0948@gmail.com>
2021-08-04 23:49:59 +0900

    amazon selenium

--- utils/selenium/sele.py
+++ utils/selenium/sele.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import os
-
-from webdriver_manager.utils import ChromeType
 
 
 __all__ = [
@@ -11,22 +7,14 @@ __all__ = [
 
 
 def get_chrome_driver(chrome_debug=False):
-    file_path = os.path.abspath(__file__)
-    driver_path = os.path.join(
-        file_path[:file_path.rfind('/') + 1],
-        "chromedriver"
-    )
+    driver_path = '/usr/local/bin/chromedriver'
     options = webdriver.ChromeOptions()
     if chrome_debug == False:
         options.add_argument('headless')
         options.add_argument('window-size=1920x1080')
         options.add_argument('--disable-gpu')
-
-    executable_path = ChromeDriverManager(
-        path=driver_path,
-        chrome_type=ChromeType.CHROMIUM).install()
     driver = webdriver.Chrome(
-        executable_path=executable_path,
+        executable_path=driver_path,
         chrome_options=options,
         service_args=['--silent'],
         service_log_path='./chrome.log'
